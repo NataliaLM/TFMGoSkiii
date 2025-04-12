7486d9a79f9a2eefd1f9eab9eafeb84922f2d64e
NataliaLM <natalia.arq.123@gmail.com>
2025-04-12 20:15:45 +0200

    Agregar archivos de proyecto.

---
+++ TFMGoSki/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TFMGoSki.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
