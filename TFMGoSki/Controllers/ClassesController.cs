d437bf3a9fdf5f295385d4189cd1271586600fca
NataliaLM <natalia.arq.123@gmail.com>
2025-04-19 08:12:46 +0200

    #11 CRUD class in NataliaLM/TFMGoSkiii#11

---
+++ TFMGoSki/Controllers/ClassesController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using TFMGoSki.Data;
+using TFMGoSki.Models;
+
+namespace TFMGoSki.Controllers
+{
+    public class ClassesController : Controller
+    {
+        private readonly TFMGoSkiDbContext _context;
+
+        public ClassesController(TFMGoSkiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Classes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Classes.ToListAsync());
+        }
+
+        // GET: Classes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Classes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // GET: Classes/Create
+        public IActionResult Create()
+        {
+            ViewBag.InstructorId = new SelectList(_context.Instructors, "Id", "Name");
+            ViewBag.ClassLevel = new SelectList(Enum.GetValues(typeof(ClassLevel)));
+            return View();
+        }
+
+        // POST: Classes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Price,StudentQuantity,ClassLevel,InstructorId")] string name, decimal price, int studentQuantity, ClassLevel classLevel, int instructorId)
+        {
+            Class @class = new Class(name, price, studentQuantity, classLevel, instructorId);
+
+            if (@class != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(@class);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return View(@class);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // GET: Classes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Classes.FindAsync(id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.InstructorId = new SelectList(_context.Instructors, "Id", "Name");
+            ViewBag.ClassLevel = new SelectList(Enum.GetValues(typeof(ClassLevel)));
+
+            return View(@class);
+        }
+
+        // POST: Classes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Price,StudentQuantity,ClassLevel,InstructorId")] string name, decimal price, int studentQuantity, ClassLevel classLevel, int instructorId)
+        {
+            Class? @class = await _context.Classes.FindAsync(id);
+            if (@class != null)
+            {                
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        @class.Update(name, price, studentQuantity, classLevel, instructorId);
+
+                        _context.Update(@class);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!ClassExists(@class.Id))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(@class);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // GET: Classes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Classes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // POST: Classes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @class = await _context.Classes.FindAsync(id);
+            if (@class != null)
+            {
+                _context.Classes.Remove(@class);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClassExists(int id)
+        {
+            return _context.Classes.Any(e => e.Id == id);
+        }
+    }
+}
--- TFMGoSki/Models/Class.cs
+++ TFMGoSki/Models/Class.cs
@@ -1,4 +1,6 @@
-﻿namespace TFMGoSki.Models
+﻿using System;
+
+namespace TFMGoSki.Models
 {
     public class Class
     {
@@ -6,5 +8,55 @@
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int StudentQuantity { get; set; }
+        public ClassLevel ClassLevel { get; set; }
+        public int InstructorId { get; set; }
+
+        public Class(string name, decimal price, int studentQuantity, ClassLevel classLevel, int instructorId)
+        {
+            Validate(name, price, studentQuantity, classLevel, instructorId);
+
+            Name = name;
+            Price = price;
+            StudentQuantity = studentQuantity;
+            ClassLevel = classLevel;
+            InstructorId = instructorId;
+        }
+
+        public Class Update(string name, decimal price, int studentQuantity, ClassLevel classLevel, int instructorId)
+        {
+            Validate(name, price, studentQuantity, classLevel, instructorId);
+
+            Name = name;
+            Price = price;
+            StudentQuantity = studentQuantity;
+            ClassLevel = classLevel;
+            InstructorId = instructorId;
+
+            return this;
+        }
+
+        private void Validate(string name, decimal price, int studentQuantity, ClassLevel classLevel, int instructorId)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("El nombre no puede ser nulo ni vacío.", nameof(name));
+            }
+            if (price <= 0)
+            {
+                throw new ArgumentException("El precio debe ser mayor que cero.", nameof(price));
+            }
+            if (studentQuantity <= 0)
+            {
+                throw new ArgumentException("La cantidad de estudiantes debe ser mayor que cero.", nameof(studentQuantity));
+            }
+            if (classLevel <= 0)
+            {
+                throw new ArgumentException("El ID del nivel de clase debe ser mayor que cero.", nameof(classLevel));
+            }
+            if (instructorId <= 0)
+            {
+                throw new ArgumentException("El ID del instructor debe ser mayor que cero.", nameof(instructorId));
+            }
+        }
     }
 }
