0f48fb06f7e5f4e1a68c00e4e49a14493e811496
NataliaLM <natalia.arq.123@gmail.com>
2025-04-19 08:40:21 +0200

    #11 CRUD Class in NataliaLM/TFMGoSkiii#11

---
+++ TFMGoSki/Models/ClassLevel.cs
@@ -0,0 +1,23 @@
+﻿namespace TFMGoSki.Models
+{
+    public enum ClassLevel
+    {
+        Beginner = 0,
+        Intermediate = 1,
+        Advanced = 2
+    }
+
+    public static class ClassLevelExtensions
+    {
+        public static string ToFriendlyString(this ClassLevel level)
+        {
+            return level switch
+            {
+                ClassLevel.Beginner => "Beginner",
+                ClassLevel.Intermediate => "Intermediate",
+                ClassLevel.Advanced => "Advanced",
+                _ => throw new ArgumentOutOfRangeException(nameof(level), level, null)
+            };
+        }
+    }
+}
