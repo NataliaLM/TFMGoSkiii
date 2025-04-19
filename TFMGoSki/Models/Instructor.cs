c3b939446c984adb8c51122caa14892c8eb1433a
NataliaLM <natalia.arq.123@gmail.com>
2025-04-19 08:35:40 +0200

    #12 CRUD Instructor in NataliaLM/TFMGoSkiii#12

---
+++ TFMGoSki/Models/Instructor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Update.Internal;
+
+namespace TFMGoSki.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Instructor(string name)
+        {
+            Validate(name);
+
+            Name = name;
+        }
+
+        public Instructor Update(string name)
+        {
+            Validate(name);
+            Name = name;
+            return this;
+        }
+
+        private void Validate(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("El nombre no puede ser nulo ni vacío.", nameof(name));
+            }
+        }
+    }
+}
