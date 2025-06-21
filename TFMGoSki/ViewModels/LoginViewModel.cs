e38dc0e21da57db9c25f70448a6986042ee63e7d
NataliaLM <natalia.arq.123@gmail.com>
2025-06-21 23:46:08 +0200

    #34 Dto and View Model of Administrator, Class Comment, Class Reservation, Client and Worker in NataliaLM/TFMGoSkiii#34

--- TFMGoSki/ViewModels/LoginViewModel.cs
+++ TFMGoSki/ViewModels/LoginViewModel.cs
@@ -4,8 +4,10 @@ namespace TFMGoSki.ViewModels
 {
     public class LoginViewModel
     {
+        [Display(Name = "User Name")]
         [Required]
         public string UserName { get; init; } = default!;
+        [Display(Name = "Password")]
         [Required, DataType(DataType.Password)]
         public string Password { get; init; } = default!;
         public bool RememberMe { get; init; }
--- TFMGoSki/ViewModels/RegisterViewModel.cs
+++ TFMGoSki/ViewModels/RegisterViewModel.cs
@@ -4,16 +4,22 @@ namespace TFMGoSki.ViewModels
 {
     public class RegisterViewModel
     {
+        [Display(Name = "Full Name")]
         [Required]
         public string FullName { get; init; } = default!;
+        [Display(Name = "Email")]
         [Required, EmailAddress]
         public string Email { get; init; } = default!;
+        [Display(Name = "Phone Number")]
         [Required, Phone]
         public string PhoneNumber { get; init; } = default!;
+        [Display(Name = "Password")]
         [Required, DataType(DataType.Password)]
         public string Password { get; init; } = default!;
+        [Display(Name = "Confirm password")]
         [DataType(DataType.Password), Compare("Password")]
         public string ConfirmPassword { get; init; } = default!;
+        [Display(Name = "Role Name")]
         [Required]
         public string RoleName { get; init; } = "Client";
     }
